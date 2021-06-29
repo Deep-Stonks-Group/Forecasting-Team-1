bd185c3aa5fea172ce09a87df1a9770be7870d44
Caleb Heinzman <caleb.heinzman@gmail.com>
2021-06-29 16:28:51 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,12 @@ setup(
     name = 'forecast-team-1',
     packages = find_packages(),
     include_package_data = True,
-    dependency_links = [
-     "git+https://github.com/Deep-Stonks-Group/PythonDataProcessing",
+    install_requires = [
+        'numpy==1.19.4',
+        'torch==1.8.1',
+        'torchvision==0.9.1',
+        'sklearn==0.0',
+        'matplotlib==3.3.3',
+        'PythonDataProcessing @ git+https://github.com/Deep-Stonks-Group/PythonDataProcessing.git',
     ],
 )
